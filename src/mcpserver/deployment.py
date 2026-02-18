fa75c5cd615517e6f4699845213284fad20b6308
Douglas Quintiliano dos Santos <douglas.q.santos@gmail.com>
2026-02-18 20:43:55 -0300

    chore: Improve the output

--- src/mcpserver/deployment.py
+++ src/mcpserver/deployment.py
@@ -7,5 +7,12 @@ mcp = FastMCP("Demo")
 # Add an addition tool
 @mcp.tool()
 def add(a: int, b: int) -> int:
-  """Add two numbers"""
-  return a + b
+  """
+  Add two numbers and return the result as a string
+  Args:
+    a: The first number (int)
+    b: The second number (int)
+  Returns:
+    The sum of the two numbers as a string (str)
+  """
+  return f"The sum of {a} and {b} is {a + b}"
